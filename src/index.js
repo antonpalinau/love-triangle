9b7949fe39121655c699bf3c754f880371cf2473
antonpalinau <antonpalinau@gmail.com>
2018-02-21 20:41:24 +0300

    solved love-triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let number = 0;
+  for (let i = 0; i < preferences.length; i++){
+    if(preferences[preferences[preferences[i]-1]-1] === i+1){
+      number +=1;
+    }
+  }
+  return Math.floor(number/3);
 };
